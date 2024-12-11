75400cae369802e60a737a26f513a6345d949b46
Kenan Günes <kguenes0@gmail.com>
2024-12-11 22:37:40 +0100

    projects and contact me

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -5,13 +5,13 @@ import { AboveTheFoldComponent } from "./above-the-fold/above-the-fold.component
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { ProjectsComponent } from "./projects/projects.component";
 import { SkillsComponent } from "./skills/skills.component"
-import { ReferencesComponent } from "./references/references.component";
 import { FooterComponent } from "../shared/footer/footer.component";
+import { ContactMeComponent } from "./contact-me/contact-me.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule ,HeaderComponent, AboveTheFoldComponent, AboutMeComponent, ProjectsComponent, SkillsComponent, ReferencesComponent, FooterComponent],
+  imports: [CommonModule, HeaderComponent, AboveTheFoldComponent, AboutMeComponent, ProjectsComponent, SkillsComponent, FooterComponent, ContactMeComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
